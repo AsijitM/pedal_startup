ce054e1dced8d2f6f63cb9f8ec8b6a7a596ff2ba
AsijitM <asijit00@gmail.com>
2024-06-22 08:38:00 +0530

    skeleton update

--- FE/src/components/SkeletonTask.jsx
+++ FE/src/components/SkeletonTask.jsx
@@ -3,9 +3,9 @@ import { Skeleton } from './ui/skeleton';
 function SkeletonTask() {
   return (
     <div className="flex flex-col gap-3">
-      <Skeleton className="w-[600px] h-[20px] rounded-full bg-gray-200" />
-      <Skeleton className="w-[600px] h-[20px] rounded-full  bg-gray-200" />
-      <Skeleton className="w-[600px] h-[20px] rounded-full  bg-gray-200" />
+      <Skeleton className="w-full h-[20px] rounded-full bg-gray-200" />
+      <Skeleton className="w-full h-[20px] rounded-full  bg-gray-200" />
+      <Skeleton className="w-full h-[20px] rounded-full  bg-gray-200" />
     </div>
   );
 }
